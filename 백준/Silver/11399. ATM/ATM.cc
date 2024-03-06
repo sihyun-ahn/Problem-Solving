66fbf03bd58acddd76140e9cf06d46551062f2ac
Sihyun <79096116+sihyun-ahn@users.noreply.github.com>
2024-03-06 20:46:17 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int n, cnt = 0;
+  cin >> n;
+  int *time = new int[n]();
+  for (int i=0; i<n; i++) {
+    cin >> time[i];
+  }
+  sort(time, time + n);
+  for (int i=0; i<n; i++) {
+    cnt += time[i] * (n - i);
+  }
+  cout << cnt;
+
+  return 0;
+}
