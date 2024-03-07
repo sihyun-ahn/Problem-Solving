bcd1454e9d8ee45f329135b7dd9503811026e39d
Sihyun <79096116+sihyun-ahn@users.noreply.github.com>
2024-03-07 22:00:11 +0900

    [Silver I] Title: IOIOI, Time: 20 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -5,18 +5,52 @@ int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  int n, m, cnt = 0;
-  string s, target = "";
-  cin >> n >> m >> s;
-  for (int i=0; i<n; i++) {
-    target += "IO";
-  }
-  target += "I";
-  for (int i = 0; i <= s.size() - (2*n + 1); i ++) {
-    if (s.substr(i, 2*n + 1) == target) {
-      cnt ++;
+  int n, m, res = 0, cnt = 0;
+  char s;
+  bool counting = false, nextI = true;
+  cin >> n >> m;
+
+  for (int i = 0; i < m; i ++) {
+    cin >> s;
+    if (counting) {
+      if (nextI) {
+        if (s == 'I') {
+          cnt ++;
+          nextI = false;
+        } else {
+          cnt --;
+          res += (cnt - n + 1 > 0) ? cnt - n + 1 : 0;
+          counting = false;
+        }
+      } else {
+        if (s == 'O') {
+          nextI = true;          
+        } else {
+          cnt --;
+          res += (cnt - n + 1> 0) ? cnt - n + 1 : 0;
+
+          counting = true;
+          nextI = false;
+          cnt = 1;
+        }
+      }
+    } else {
+      if (nextI && s == 'I') {
+        counting = true;
+        cnt = 1;
+        nextI = false;
+      } else {
+        nextI = true;
+        cnt = 0;
+      }
     }
   }
-  cout << cnt;
+
+  if (counting) {
+    cnt --;
+    res += (cnt - n + 1> 0) ? cnt - n + 1 : 0;
+  }
+
+  cout << res;
   return 0;
 }
