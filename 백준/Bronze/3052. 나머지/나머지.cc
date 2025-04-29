f0f593fc7478d540bc0e68f323fca309dc166652
Sihyun <79096116+sihyun-ahn@users.noreply.github.com>
2025-04-29 23:38:11 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.cc
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -2,15 +2,11 @@
 #include <set>
 using namespace std;
 
-int gcd(int a, int b) {
-  return b ? gcd(b, a%b) : a;
-}
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  int n, denom, num, divider;
+  int n;
   set<int> s;
   for (int i=0; i<10; i++) {
     cin >> n;
